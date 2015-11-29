7ef3db2faa3a521a71c9202ccbf1775b74b2bcdf
Chris David <chrisdavid@chriss-mbp.home>
2015-11-29 17:52:19 -0500

    Initial Commit

---
+++ Ships/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ships
+//
+//  Created by Chris David on 11/29/15.
+//  Copyright © 2015 Chris David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ships/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ships
+//
+//  Created by Chris David on 11/29/15.
+//  Copyright © 2015 Chris David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
